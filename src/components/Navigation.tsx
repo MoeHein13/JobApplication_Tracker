1be7cdb4ff9280dbb2fdfc21b5a9ae1b0fb2223f
Kelvin <kelvinmoe96@gmail.com>
2025-12-17 23:27:01 +0800

    edit navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,19 +1,18 @@
 const Navigation = () => {
   return (
-    <div className="max-w-7x border-2 rounded-2xl shadow-md bg-white grid grid-cols-1 sm:grid-cols-2 p-5 justify-between gap=3">
+    <div className="m-6 max-w-7xl border border-white rounded-2xl shadow-md bg-white flex p-5 flex-wrap justify-between gap-3 ">
       <div className="">
         <h1 className="text-3xl font-bold ">Job Application Tracker</h1>
         <span>Total Applications : 20</span>
       </div>
-      <div className="">
-        <div className="flex  items-center gap-3">
-          <button className=" border border-blue-300 rounded-2xl p-3 text-blue-300">
-            Export to Excel
-          </button>
-          <button className="bg-blue-300 text-white font-semibold rounded-2xl p-3">
-            Add Application
-          </button>
-        </div>
+
+      <div className="flex justify-between items-center gap-3">
+        <button className="bg-blue-300 text-white font-semibold rounded-xl px-5 py-2 text-2xl ">
+          Add Application
+        </button>
+        <button className=" border border-blue-300 rounded-xl px-5 py-2 text-blue-300 text-2xl">
+          Export to Excel
+        </button>
       </div>
     </div>
   );
