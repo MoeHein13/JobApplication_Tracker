c0d048bf58d54b979ae7dfbb4a1e2260c5925099
Kelvin <kelvinmoe96@gmail.com>
2026-01-02 23:32:27 +0800

    edit the application new form

--- src/components/JobModal.tsx
+++ src/components/JobModal.tsx
@@ -1,6 +1,6 @@
 import { useSetAtom } from "jotai";
 import { uiModal } from "../atoms/modalatom";
-
+import { X } from "lucide-react";
 const JobModal = () => {
   const setModal = useSetAtom(uiModal);
   const closeModal = () => {
@@ -13,11 +13,39 @@ const JobModal = () => {
     >
       <div className="absolute inset-0 bg-black/40 backdrop-blur-sm" />
       <div
-        className="relative w-full max-w-lg rounded-lg bg-white p-6 shadow-xl"
+        className="relative w-full max-w-lg rounded-lg bg-white p-6 shadow-xl flex flex-col justify-center"
         onClick={(e) => e.stopPropagation()}
       >
-        <h1>Job Application Adding</h1>
-        <button onClick={closeModal}>Close</button>
+        <div className="flex items-center justify-between">
+          <h1>Job Application Adding</h1>
+          <button
+            onClick={closeModal}
+            className="cursor-pointer  p-2 hover:bg-gray-200 hover:rounded-full"
+          >
+            <X />
+          </button>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col justify-center gap-2 w-full m-3">
+            <label htmlFor="company">Company</label>
+            <input
+              type="text"
+              placeholder="eg.Acme Corp"
+              id="company"
+              className="outline-1 border-gray-300 rounded-md p-2 text-gray-400"
+            />
+          </div>
+          <div className="flex flex-col justify-center gap-2 w-full m-3">
+            <label htmlFor="position">Position</label>
+            <input
+              type="text"
+              placeholder="eg.Frontend Engineer"
+              id="position"
+              className="outline-1 border-gray-300 rounded-md p-2 text-gray-400"
+            />
+          </div>
+        </div>
       </div>
       {/* <div className="rounded-xl p-5 border border-gray-400 max-w-72 ">
       </div> */}
--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -1,12 +1,12 @@
 import { createPortal } from "react-dom";
 import JobModal from "../components/JobModal";
-import { useAtom } from "jotai";
+import { useAtomValue } from "jotai";
 import { uiModal } from "../atoms/modalatom";
 
 const mountElement = document.getElementById("portal");
 
 const Modal = () => {
-  const [ui] = useAtom(uiModal);
+  const ui = useAtomValue(uiModal);
 
   return createPortal(<> {ui.modal && <JobModal />}</>, mountElement!);
 };
