fa3dd3fb8e25dbd49d84080de940e39ea1c47988
Kelvin <kelvinmoe96@gmail.com>
2026-01-04 23:55:04 +0800

    add the types for the appication form

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export type Application = {
+  id: string;
+  company: string;
+  position: string;
+  status: string;
+  dataApplied: string;
+  salary?: string;
+  location?: string;
+  notes?: string;
+  url?: string;
+};
