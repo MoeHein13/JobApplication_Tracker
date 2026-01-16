fee408ab20255ec82ed9b8d608d8f10582834e12
Kelvin <kelvinmoe96@gmail.com>
2026-01-16 23:58:51 +0800

    render the list from the application form

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation";
 import SearchFiltler from "./components/SearchFiltler";
 import Modal from "./ui/Modal";
 import { uiModal } from "./atoms/modalatom";
+import JobList from "./components/JobList";
 
 const App = () => {
   const modal = useAtomValue(uiModal);
@@ -10,6 +11,9 @@ const App = () => {
     <div className=" min-h-screen flex flex-col  items-center ">
       <Navigation />
       <SearchFiltler />
+      <div className="w-full max-w-7xl px-6 pb-8">
+        <JobList />
+      </div>
       {modal && <Modal />}
     </div>
   );
